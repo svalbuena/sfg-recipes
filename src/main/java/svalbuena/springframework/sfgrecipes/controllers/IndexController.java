33019df810ec47cca436aad271229d8ad1c79cc5
svalbuena <sergivalbuena@hotmail.com>
2021-04-01 19:51:58 +0200

    Add Index Page and Controller

---
+++ src/main/java/svalbuena/springframework/sfgrecipes/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package svalbuena.springframework.sfgrecipes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping({"", "/"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
