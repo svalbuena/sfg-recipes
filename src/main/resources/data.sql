3e94e0df6548fd31c32ef7e1c788553205a08002
svalbuena <sergivalbuena@hotmail.com>
2021-04-02 23:06:22 +0200

    Add SQL data import script for Spring Boot

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Spanish');
+INSERT INTO category (description) VALUES ('French');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Grams');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Cups');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Ounces');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Liters');
